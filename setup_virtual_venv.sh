2712129fa71bb4bc0ca3cfb6e8eae21bae9e6feb
Michael <76454137+mtrimblett@users.noreply.github.com>
2024-02-11 18:40:23 +1100

    Update setup_virtual_venv.sh

--- setup_virtual_venv.sh
+++ setup_virtual_venv.sh
@@ -1,4 +1,4 @@
-./opt/Python-2.7.15/configure --prefix=$HOME/opt/python-2.7.15"
+./opt/Python-2.7.15/configure --prefix=$HOME/opt/python-2.7.15
 make
 make install
 virtualenv -p /home/michael/opt/python-2.7.15/bin/python venv
